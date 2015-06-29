f548d11159b6f16814c489e424831c7cf25ab27c
Ethan Hathaway <erhathaway@gmail.com>
2015-06-28 22:42:17 -0400

    clean up and fix code to interleave image

--- image.rb
+++ image.rb
@@ -1,48 +1,35 @@
-# require 'rubygems'
 require 'rmagick'
-require 'net/http'
-# require 'json'
 require 'open-uri'
 require 'pry'
 
 
     def fetch_image(url)
-      # LOGGER.debug "Open URL"
       file = open(url)
-
       img = Magick::ImageList.new
-      # LOGGER.debug "Read blob"
       img.from_blob(file.read)
-      # img.write('test.jpg')
     end
 
     def transform_image(img)
-      # Owls tend to be brownish -> boost red and green channels
-      # img = img.normalize_channel(Magick::RedChannel)
-      # img = img.normalize_channel(Magick::GreenChannel)
+      # enchance black / white levels
       img = img.level_channel(Magick::RedChannel, black_point = 0, white_point = 50000, gamma = 2)
       img = img.level_channel(Magick::GreenChannel, black_point = 0, white_point = 50000, gamma = 2)
 
       # Reduce size and number of colors
       @size = 120
-      img = img.resize_to_fit(@size, @size)
+      @size2 = 120
+      img = img.resize_to_fit(@size, @size2)
       img = img.posterize(10)
 
       img = img.quantize(5, Magick::GRAYColorspace)
     end
 
-    # OUTPUT_ENTITIES = ['&#9608;', '&#9619;', '&#9617;', "&nbsp;"]
-    # White and black, respectively
-    # OUTPUT_ENTITIES = [0, 0000, 60000, 60000]
-    OUTPUT_ENTITIES = [ " " , " " , "X", "X"]
-    # OUTPUT_ENTITIES = [ "X" , "X" , " ", " "]
+
+    OUTPUT_ENTITIES = [ " " , " " , "#", "#"]
 
     def translate_image(img)
       # Export image as list of pixels
       pixels = img.export_pixels(0, 0, img.columns, img.rows, 'I')
 
-      # pixels_new.write('hi.jpg')
-      # Find all unique pixel values
       values = {}
       pixels.group_by do |x|
         values[x] = 0 unless values.has_key? x
@@ -52,10 +39,7 @@ require 'pry'
       values.keys.sort.each {|key| values[key] = i; i = i + 1 }
 
       # Map pixel rank to html entities.
-      a = pixels.map { |pixel| OUTPUT_ENTITIES[values[pixel]] }
-      a
-      # Dump picture to black and white
-      # pixels_new = img.import_pixels(0, 0, img.columns, img.rows, 'I',a)
+      pixels.map { |pixel| OUTPUT_ENTITIES[values[pixel]] }
     end    
 
 
@@ -72,27 +56,58 @@ require 'pry'
       file.close
     end    
 
+
+    def write_other_line(file_object, code_line, image_array, line_length)
+      file_object.write(code_line)
+      if image_array.length > 0
+        image_line = image_array.shift(line_length).join
+      else
+        image_line = '#'*line_length
+      end
+      file_object.write(image_line + "\n")
+    end
+
     def write_line(file_object, code_line, image_array, line_length)
       new_image_string = ""
       column_count = 0
       line_stop = false
+      code_end_flag = false
 
+      #scan through image array until end of line
       while column_count < line_length
+
+        #get image pixel sequentially
         pixel = image_array.shift
+
+        #if pixel is not blank and end of code (aka line_stop) is not reached
         if pixel != " " and line_stop == false # or pixel != nil
+
+          #if there is code, substitute it in for the pixel
           if code_line.length > 0
             a = " "
+            #but don't take spaces or new lines
             while a == " " or a == "\n"
               a = code_line.shift
             end
             pixel = a
+
+          #if there is not code, substitue in a comment symbol
           else
             pixel = '#'
           end
+
+        #if the pixel is not blank and the end of code has been reached
+        #substitute in a comment symbol
         elsif pixel != " " and line_stop == true
           pixel = '#'
-        else
+
+
+        elsif code_end_flag == false
           line_stop = true
+          code_end_flag = true
+          code_line.unshift(new_image_string[-1])
+          new_image_string.chop!
+          new_image_string += '\\'
         end
         if pixel == nil 
           pixel = " "
@@ -108,72 +123,30 @@ require 'pry'
       end
     end
 
-    def replace(ruby_file, output_file, image_array, line_length)
+    def replace(ruby_file, output_file, image_array, line_length, mode=1)
 
       save_file = open(output_file, 'w')
       code_array = open(ruby_file, 'r') do |f|
         f.each_line do |line|
-          code_line = line.split(/' '/)
-          extra_code = true
-          while extra_code
-            extra_code = write_line(save_file, code_line, image_array, line_length)
+          if mode ==0
+            code_line = line.split("")
+            extra_code = true
+            while extra_code
+              extra_code = write_line(save_file, code_line, image_array, line_length)
+            end
+          else
+            write_other_line(save_file, line, image_array, line_length)
           end
         end
       end
       save_file.close
     end
 
-
-    # image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXSK2aQsa7H7xPoUtvLrwK-XoS3wBASrq0fP-IZ3CtrcgchdCg_w'
-    # image = 'http://1.bp.blogspot.com/-aRsKTxxGdR8/U9lKeDKOaCI/AAAAAAAABSQ/47oVl8gby6U/s1600/Pringles+guy+snip.PNG'
     image = 'http://theartmad.com/wp-content/uploads/2015/04/Smiley-Face-2-300x300.jpg?timestamp=1435364618'
-    # image = 'http://previews.123rf.com/images/rahultiwari3190/rahultiwari31901006/rahultiwari3190100600616/7266824-Computer-hand-cursor-for-click-in-middle-finger-vector-illustartion--Stock-Vector.jpg'
+
     image1 = fetch_image(image)
-    # image1.ordered_dither(threshold_map='4x4') .write('test5.jpg')
-    image2 = transform_image(image1).write('hi.jpg')
+    image2 = transform_image(image1)
     image3 = translate_image(image2)
-    # write('testfile.txt',image3,image2.columns)
-    # puts image3
 
-    # image1.posterize(10).quantize(5, Magick::GRAYColorspace).write('test2.jpg')
+    replace('image.rb', 'test_file_code.rb', image3, image2.columns)
 
-    replace('image.rb', 'testfile_code.txt', image3, image2.columns)
-
-
-    # def replace(ruby_file, output_file, image_array, line_length)
-
-    #   save_file = open(output_file, 'w')
-    #   code_array = open(ruby_file, 'r') do |f|
-    #     f.each_line do |line|
-    #       code_line = line.split("")
-    #   # binding.pry
-    #       new_image_string = ""
-    #       column_count = 0
-    #       counter = 0
-    #       while column_count < line_length
-    #         pixel = image_array.shift
-    #         if pixel != " " # or pixel != nil
-    #           if code_line.length > 0
-    #             a = " "
-    #             while a == " " or a == "\n"
-    #               a = code_line.shift
-    #             end
-    #             pixel = a
-    #           else
-    #             pixel = '#'
-    #           end
-    #         end
-    #         if pixel == nil 
-    #           pixel = " "
-    #         end
-    #         new_image_string += pixel
-    #         column_count += 1
-    #       end
-    #         # binding.pry
-    #       counter +=1
-    #       save_file.write(new_image_string + "\n")
-    #       column_count = 0
-    #     end
-    #   end
-    #   save_file.close
-    # end
